6867163f9333398bc98bc2424db6c323296d8f96
nawzneen <earworm90@gmail.com>
2024-10-16 12:05:35 +0300

    Feat: create layout in tabs folder

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,73 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      // screenOptions={{
+      //   tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      //   headerShown: false,
+      // }}
+      screenOptions={{
+        tabBarActiveTintColor: "orange", // Set the active tab color to orange
+        tabBarInactiveTintColor: "gray", // Set the inactive tab color to gray
+        headerShown: false, // Hide the header for the tabs
+      }}
+    >
+      {/* <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={color}
+            />
+          ),
+        }}
+      /> */}
+      <Tabs.Screen
+        name="test"
+        options={{
+          title: "Test",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "code-slash" : "code-slash-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="connect"
+        options={{
+          title: "Connect",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "code-slash" : "code-slash-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="results"
+        options={{
+          title: "Results",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "code-slash" : "code-slash-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
