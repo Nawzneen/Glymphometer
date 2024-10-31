3f37a3e97384f018811372bad9bd5ff963c38007
nawzneen <earworm90@gmail.com>
2024-10-31 13:32:17 +0200

    Feat: add use effects to initialize ble state listner and reconnection logic

--- utils/useBLE.ts
+++ utils/useBLE.ts
@@ -1,29 +1,63 @@
 // useBLE.native.ts
-import { useState } from "react";
+////////////////////////////////////////////////
+import * as FileSystem from "expo-file-system";
+import { useEffect, useState } from "react";
 import { PermissionsAndroid, Platform } from "react-native";
-import {
-  discoverBLEServicesCharactristics,
-  readBatteryLevel,
-} from "./bleCharacteristics";
+import { discoverBLEServicesCharactristics } from "./bleCharacteristics";
 import * as ExpoDevice from "expo-device";
 import base64 from "react-native-base64";
-import {
-  BleError,
-  BleManager,
-  Device,
-  Characteristic,
-} from "react-native-ble-plx";
+import { BleError, Device, Characteristic } from "react-native-ble-plx";
+import bleManager from "./bleManager";
+import { Alert } from "react-native";
 
 // Device Characteristitcs UUIDs specific to GM5
 const DATA_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"; // Service UUID for handling data
-const RX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"; //To Send Data
-const TX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"; // To Recieve UUID
-
-const bleManager = new BleManager();
+const RX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"; //To Send Data / write
+const TX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"; // To Recieve UUID / notify
 
 function useBLE() {
   const [allDevices, setAllDevices] = useState<Device[]>([]); //Track all discovered devices
   const [connectedDevice, setConnectedDevice] = useState<Device | null>(null); //Track currently connected device
+  // Initialization and BLE State Listener
+  useEffect(() => {
+    const subscription = bleManager.onStateChange((state) => {
+      console.log("BLE State:", state);
+      if (state === "PoweredOn") {
+        // Optionally start scanning automatically
+        // scanForPeripherals();
+      } else if (state === "PoweredOff") {
+        Alert.alert(
+          "Bluetooth Disabled",
+          "Please enable Bluetooth to connect to your GM5 device.",
+          [{ text: "OK" }]
+        );
+      }
+    }, true);
+
+    return () => {
+      // Cleaning up BLE Manager without destroying it
+      subscription.remove();
+    };
+  }, []);
+
+  // Reconnection Logic on Mount
+  // Fetch already connected devices on mount (to prevent hot reloads destroy the connection without disconnecting ble device)
+  useEffect(() => {
+    const fetchConnectedDevices = async () => {
+      try {
+        const devices = await bleManager.connectedDevices([DATA_SERVICE_UUID]);
+        console.log("Connected devicess:", devices);
+        if (devices.length > 0) {
+          setConnectedDevice(devices[0]);
+          allDevices.push(devices[0]);
+          console.log("Found connected devicees:", devices[0].id);
+        }
+      } catch (error) {
+        console.error("Error fetching connected devices:", error);
+      }
+    };
+    fetchConnectedDevices();
+  }, []);
 
   //Requesting Android-specific Location Permission (Android 12 and later require additional permissions)
   const requestAndroid31Permissions = async () => {
@@ -91,7 +125,7 @@ function useBLE() {
     }
   };
 
-  //Function to connect to a BLE device
+  //FUNCTION TO CONNECT TO THE BLE DEVICE
   const connectToDevice = async (device: Device) => {
     try {
       // Connect to the device using its ID
@@ -100,9 +134,6 @@ function useBLE() {
 
       // Discover all services and characteristics of the device
       await discoverBLEServicesCharactristics(deviceConnection);
-
-      // //Read the battery level of the device
-      // await readBatteryLevel(deviceConnection);
 
       // set the connected device state
       setConnectedDevice(deviceConnection);
@@ -113,24 +144,29 @@ function useBLE() {
       console.error(error);
     }
   };
+
+  //FUNCTION TO DISCONNECT TO THE BLE DEVICE
   const disconnectDevice = async () => {
-    if (connectedDevice) {
-      try {
-        await bleManager.cancelDeviceConnection(connectedDevice.id); // Disconnect the device
-        setConnectedDevice(null); // Reset the connected device
-        console.log("Device Disconnected successfully");
-      } catch (error) {
-        console.log("FAILED TO DISCONNECT", error);
-      }
+    console.log("Disconnecting device running");
+    if (!connectedDevice) {
+      console.log("No device connected to disconnect");
+      return;
+    }
+    try {
+      await bleManager.cancelDeviceConnection(connectedDevice.id); // Disconnect the device
+      setConnectedDevice(null); // Reset the connected device
+      console.log("Device Disconnected successfully");
+    } catch (error) {
+      console.log("FAILED TO DISCONNECT", error);
     }
   };
 
-  //Helper function to check if a device has already been discovered
+  //HELPER FUNCTION TO CHECK IF A DEVICE HAS ALREADY BEEN DISCORVERED
   const isDeviceDuplicated = (devices: Device[], newDevice: Device) => {
     return devices.findIndex((device) => newDevice.id === device.id) > -1;
   };
 
-  //Function to start scanning for BLE devices
+  //FUNCTION TO START SCANNING FOR PERIPHERALS
   const scanForPeripherals = () => {
     bleManager.startDeviceScan(null, null, (error, device) => {
       if (error) {
@@ -154,7 +190,7 @@ function useBLE() {
   };
   ////
 
-  //Function to start or pause data streaming, specific to GM5
+  //FUNCTION TO TOGGLE DATA STREAMING, SPECIFIC TO GM5
   // S starts the data streaming, P pauses the data streaming
   const startOrPauseDataStreaming = async (device: Device, command: string) => {
     const status = command === "S" ? "Start" : "Pause";
@@ -176,7 +212,7 @@ function useBLE() {
     }
   };
 
-  // Function data Streaming
+  // FUNCTION FOR DATA STREAMING
   const dataStreaming = (
     device: Device,
     setReceivedData?: (data: string) => void
@@ -188,7 +224,6 @@ function useBLE() {
       device
         .requestMTU(512)
         .then((mtu) => {
-          // console.log("MTU size value", mtu.mtu);
           device.monitorCharacteristicForService(
             DATA_SERVICE_UUID,
             TX_CHARACTERISTIC_UUID,
@@ -201,7 +236,7 @@ function useBLE() {
     }
   };
 
-  //Callback function to handle the data
+  //CALL BACK FUNCTION TO HANDLE THE RECEIVED DATA
   const onDataUpdate =
     (setReceivedData?: (data: string) => void) =>
     (error: BleError | null, characteristic: Characteristic | null) => {
