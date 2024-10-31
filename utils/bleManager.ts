f53c3010689a44cf162fbe6c98ad0a3768e2221a
nawzneen <earworm90@gmail.com>
2024-10-31 12:43:25 +0200

    chore: change a comment

--- utils/bleManager.ts
+++ utils/bleManager.ts
@@ -1,4 +1,4 @@
-// bleManager.js
+//Create BleManager as a single instance throughout app to maintain consistent state.
 import { BleManager } from "react-native-ble-plx";
 const bleManager = new BleManager();
 export default bleManager;
