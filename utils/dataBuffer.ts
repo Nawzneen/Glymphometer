0097c58df735d8275902f837524ec76dc52c523b
nawzneen <earworm90@gmail.com>
2024-11-08 17:02:49 +0200

    Bug: replaced concat method with push method to imrpove app performance

--- utils/dataBuffer.ts
+++ utils/dataBuffer.ts
@@ -3,7 +3,11 @@ let dataBuffer: number[] = [];
 export const getDataBuffer = () => dataBuffer;
 
 export const addToDataBuffer = (dataChunk: number[]) => {
-  dataBuffer = dataBuffer.concat(dataChunk);
+  try {
+    dataBuffer.push(...dataChunk);
+  } catch (error) {
+    console.log("Error adding data to buffer", error);
+  }
 };
 
 export const clearDataBuffer = () => {
