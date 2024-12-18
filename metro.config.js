7988a90287ce9a7b1e379aa2cba5befe1917b0e8
nawzneen <earworm90@gmail.com>
2024-12-18 16:00:38 +0200

    Chore: comment out unused metro.config.js code for clarity

--- metro.config.js
+++ metro.config.js
@@ -5,3 +5,15 @@ const config = getDefaultConfig(__dirname);
 config.resolver.assetExts.push("txt");
 
 module.exports = config;
+
+// const { getDefaultConfig } = require("@react-native/metro-config");
+
+// const defaultConfig = getDefaultConfig(__dirname);
+
+// module.exports = {
+//   ...defaultConfig,
+//   transformer: {
+//     ...defaultConfig.transformer,
+//     unstable_allowRequireContext: true,
+//   },
+// };
